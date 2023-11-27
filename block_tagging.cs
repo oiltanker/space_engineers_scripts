ff584d3770a7a6caf2b122d5de7b1dc3e2e55601
Oiltanker <OiltankerDK@gmail.com>
2023-11-27 19:03:12 +0200

    small additions to cvb+ & tagging

--- block_tagging.cs
+++ block_tagging.cs
@@ -7,4 +7,5 @@ public void Main(string argument, UpdateType updateSource) {
     GridTerminalSystem.GetBlocks(blocks);
 
     blocks.Where(b => b is IMyGyro && b.CubeGrid == Me.CubeGrid).ToList().ForEach(b => (b as IMyGyro).CustomName += " @cdrive");
+    // blocks.Where(b => b is IMyBatteryBlock && b.CubeGrid == Me.CubeGrid).ToList().ForEach(b => (b as IMyBatteryBlock).CustomName = "battery @s2b-static");
 }
